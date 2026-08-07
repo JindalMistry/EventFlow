8a4817106f7f5848f6ab070d613568cc1741eee7
Jindal Mistry <mistryjindal@gmail.com>
2026-08-07 21:45:23 +0530

    BaseEntity Added

---
+++ EventFlow.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+namespace EventFlow.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid? CreatedByUserId { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public Guid? UpdatedByUserId { get; set; }
+    public bool IsDeleted { get; set; }
+}
