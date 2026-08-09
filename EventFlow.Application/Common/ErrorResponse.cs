c312df50ea425e70a0e5c675fcd393fab7b7a01c
Jindal Mistry <mistryjindal@gmail.com>
2026-08-09 10:02:23 +0530

    Common Response DTOs

---
+++ EventFlow.Application/Common/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EventFlow.Application.Common
+{
+    public class ErrorResponse
+    {
+        public bool Success { get; set; } = false;
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
