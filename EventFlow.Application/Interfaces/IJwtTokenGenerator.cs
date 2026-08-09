5cdcfa0f10a5e8a3384ece6af920ef175b16a481
Jindal Mistry <mistryjindal@gmail.com>
2026-08-09 10:01:03 +0530

    JWT and Password Interfaces

---
+++ EventFlow.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using EventFlow.Domain.Entities;
+
+namespace EventFlow.Application.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ EventFlow.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace EventFlow.Application.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        bool Verify(string password, string hash);
+    }
+}
