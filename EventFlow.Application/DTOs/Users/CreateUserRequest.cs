4963a9eea8b2ce09b9b6b23c26de40776cf0c980
Jindal Mistry <mistryjindal@gmail.com>
2026-08-10 07:46:48 +0530

    Create User Modal

---
+++ EventFlow.Application/DTOs/Users/CreateUserRequest.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventFlow.Application.DTOs.Users;
+
+public class CreateUserRequest
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(256)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public Guid ApplicationId { get; set; }
+}
