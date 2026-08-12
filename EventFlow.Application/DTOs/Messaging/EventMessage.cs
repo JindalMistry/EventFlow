69f302cbac7a720f5a6608e603b91f981b560ecb
Jindal Mistry <mistryjindal@gmail.com>
2026-08-12 20:46:09 +0530

    Messaging DTOs

---
+++ EventFlow.Application/DTOs/Messaging/EventMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace EventFlow.Application.Messaging;
+
+public sealed record EventMessage(
+    Guid PublishedEventId,
+    Guid ApplicationId,
+    Guid EventDefinitionId,
+    Guid CorrelationId
+);
---
+++ EventFlow.Application/DTOs/Messaging/NotificationMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventFlow.Application.DTOs.Messaging
+{
+    public class NotificationMessage
+    {
+        public Guid NotificationId { get; set; }
+    }
+}
