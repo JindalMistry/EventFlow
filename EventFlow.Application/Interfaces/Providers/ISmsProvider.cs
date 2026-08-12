f16661001b0fd216cb476550cda805322318d004
Jindal Mistry <mistryjindal@gmail.com>
2026-08-12 20:48:37 +0530

    Provider Interfaces

---
+++ EventFlow.Application/Interfaces/Providers/ISmsProvider.cs
@@ -0,0 +1,11 @@
+﻿using EventFlow.Application.Interfaces.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventFlow.Application.Interfaces.Providers
+{
+    public interface ISmsProvider: INotificationProvider
+    {
+    }
+}
