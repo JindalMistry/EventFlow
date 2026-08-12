ad65fd3c6ed50430a607d2459ca6a2c14994482a
Jindal Mistry <mistryjindal@gmail.com>
2026-08-12 20:45:59 +0530

    Template Engine DTOs

---
+++ EventFlow.Application/DTOs/TemplateEngine/RenderedTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace EventFlow.Application.DTOs.TemplateEngine
+{
+    public sealed class RenderedTemplate
+    {
+        public string? Subject { get; init; }
+        public string Body { get; init; } = string.Empty;
+    }
+}
