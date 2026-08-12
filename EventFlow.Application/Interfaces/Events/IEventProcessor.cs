84762affe169a8fc2dd9a8856cb79a41911bf058
Jindal Mistry <mistryjindal@gmail.com>
2026-08-12 20:48:24 +0530

    Events Interfaces

---
+++ EventFlow.Application/Interfaces/Events/IEventProcessor.cs
@@ -0,0 +1,10 @@
+﻿using EventFlow.Application.Messaging;
+
+namespace EventFlow.Application.Interfaces.Events;
+
+public interface IEventProcessor
+{
+    Task ProcessAsync(
+        EventMessage message,
+        CancellationToken cancellationToken = default);
+}
